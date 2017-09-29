751d6130e1e64d383aced0dcf45628bf3f984dcb
Ethan Rockwood <de-rockwood@wiu.edu>
2017-09-29 13:07:18 -0500

    Update SQL Database instruction in README and provide SQL Schema project

---
+++ FSRMTrendsDB/dbo/Tables/Sizes.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Sizes](
+	[QuotaPaths] [nvarchar](255) NOT NULL,
+	[Date] [datetime] NOT NULL,
+	[Size] [numeric](38, 0) NULL,
+ CONSTRAINT [PK_Sizes] PRIMARY KEY CLUSTERED 
+(
+	[QuotaPaths] ASC,
+	[Date] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Sizes]  WITH CHECK ADD  CONSTRAINT [FK_Sizes_Dates] FOREIGN KEY([Date])
+REFERENCES [dbo].[Dates] ([Date])
+GO
+
+ALTER TABLE [dbo].[Sizes] CHECK CONSTRAINT [FK_Sizes_Dates]
+GO
+ALTER TABLE [dbo].[Sizes]  WITH CHECK ADD  CONSTRAINT [FK_Sizes_QuotaPaths] FOREIGN KEY([QuotaPaths])
+REFERENCES [dbo].[QuotaPaths] ([QuotaPath])
+GO
+
+ALTER TABLE [dbo].[Sizes] CHECK CONSTRAINT [FK_Sizes_QuotaPaths]
---
+++ FSRMTrendsDB/dbo/Tables/Usages.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Usages](
+	[QuotaPath] [nvarchar](255) NOT NULL,
+	[Date] [datetime] NOT NULL,
+	[UsageBytes] [numeric](38, 0) NULL,
+ CONSTRAINT [PK_Usages] PRIMARY KEY CLUSTERED 
+(
+	[QuotaPath] ASC,
+	[Date] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Usages]  WITH CHECK ADD  CONSTRAINT [FK_Usages_Dates] FOREIGN KEY([Date])
+REFERENCES [dbo].[Dates] ([Date])
+GO
+
+ALTER TABLE [dbo].[Usages] CHECK CONSTRAINT [FK_Usages_Dates]
+GO
+ALTER TABLE [dbo].[Usages]  WITH CHECK ADD  CONSTRAINT [FK_Usages_QuotaPaths] FOREIGN KEY([QuotaPath])
+REFERENCES [dbo].[QuotaPaths] ([QuotaPath])
+GO
+
+ALTER TABLE [dbo].[Usages] CHECK CONSTRAINT [FK_Usages_QuotaPaths]
